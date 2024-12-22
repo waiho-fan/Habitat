471eb8242e74b5904697327b9e4a467ba15b10c2
Gary Fan <tomfan419@gmail.com>
2024-12-22 10:14:31 +0800

    Initial Commit

---
+++ Habita/HabitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitaApp.swift
+//  Habita
+//
+//  Created by Gary on 22/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
